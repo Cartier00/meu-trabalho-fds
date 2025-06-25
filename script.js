9c7f5dcb6b765f8b9fcd48ec459329f6b7788663
Cartier00 <jpdeaquinomacielsilva77@gmail.com>
2025-06-25 18:39:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Vermensagem() {
+function verMensagem() {
   const div = document.getElementById("mensagem");
   div.textContent = "Bem vindo a minha página, pode ser simples, mas fiz com carinho e espero que goste, pois usei tudo o que aprendi até agora, obrigado ;)";
 }
