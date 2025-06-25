cade9b472a7ea6ccea70bb4187e4d15aff08e451
Cartier00 <jpdeaquinomacielsilva77@gmail.com>
2025-06-25 17:46:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function Vermensagem() {
+  const div = document.getElementById("mensagem");
+  div.textContent = "Bem vindo a minha página, pode ser simples, mas fiz com carinho e espero que goste, pois usei tudo o que aprendi até agora, obrigado ;)";
+}
